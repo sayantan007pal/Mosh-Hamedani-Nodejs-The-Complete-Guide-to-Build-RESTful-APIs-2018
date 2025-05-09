5c4afc5531a74a24d60baf00dc348983d6453f3a
sayantan007pal <sayantanpal100@gmail.com>
2025-05-09 16:46:57 +0530

    section:2 -> 12, 13, 14 - all files

---
+++ _2_Node_Module_system/logger.js
@@ -0,0 +1,7 @@
+var url = 'https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox/FMfcgzQbfBmrXJsDgGRCNtQfCjWKlwxH'  ;
+
+function show(val){
+    console.log('message')
+}
+module.exports.endpoint = url;
+module.exports.func = show;
