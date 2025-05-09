540eef6c36ac885b77aabe1f96c4973ee8c708f7
sayantan007pal <sayantanpal100@gmail.com>
2025-05-10 04:27:33 +0530

    15 module wrapper function

--- _2_Node_Module_system/logger.js
+++ _2_Node_Module_system/logger.js
@@ -1,3 +1,5 @@
+console.log(__filename)  // file's path from where it is imported
+console.log(__dirname) //  the file's path on which the import is done
 var url = 'https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox/FMfcgzQbfBmrXJsDgGRCNtQfCjWKlwxH'  ;
 
 function show(val){
