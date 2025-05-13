bf4f157a6e6947fb156fdba4fe7ad7cbeb179d56
sayantan007pal <sayantanpal100@gmail.com>
2025-05-13 18:57:53 +0530

    21. extending event emitter---Refactor event listener in app.js and logger.js for improved clarity and functionality

--- _2_Node_Module_system/app.js
+++ _2_Node_Module_system/app.js
@@ -4,9 +4,18 @@ const emitter = new EventEmitter(); //event is a object like - John i.e human na
 
 
 //register a listener
-emitter.on('messageLogged', function(){
-    console.log("Listener called ");  //"Listener called " output signifies when " emitter.emit('messageLogged', {id:1, url: 'http://'}) " this event is raised , " emitter.on('messageLogged', function() " this callback function is called
+emitter.on('messageLogged', (arg)=>{
+    console.log("Listener called ", arg);  //"Listener called " output signifies when " emitter.emit('messageLogged', {id:1, url: 'http://'}) " this event is raised , " emitter.on('messageLogged', function() " this callback function is called
 })//emitter.addListener('Name of event','Callback function or Listener') same as emitter.on()
 
-//raised an event
-emitter.emit('messageLogged', {id:1, url: 'http://'})//"event arguments are stored in a object"
+
+/*
+but here in this case the event listner above was not called since we are calling event listner from logger.js file
+
+*/
+
+
+const log = require("./logger");
+// log(); //no output
+log();      //message
+log("messag");  //message
--- _2_Node_Module_system/logger.js
+++ _2_Node_Module_system/logger.js
@@ -1,9 +1,14 @@
-console.log(__filename)  // file's path from where it is imported
-console.log(__dirname) //  the file's path on which the import is done
+const EventEmitter = require("events"); //EventEmitter is a class like -Human and 'E' in EventEmitter in the beginning signifies it is a "Class"
+
+const emitter = new EventEmitter(); //event is a object like - John i.e human name
 var url = 'https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox/FMfcgzQbfBmrXJsDgGRCNtQfCjWKlwxH'  ;
 
 function show(val){
+    //Send an HTTP request
     console.log('message')
+
+    //raised an event
+    emitter.emit('messageLogged', {id:1, url: 'http://'})//"event arguments are stored in a object"
+
 }
-module.exports.endpoint = url;
-module.exports.func = show;
+module.exports = show;
