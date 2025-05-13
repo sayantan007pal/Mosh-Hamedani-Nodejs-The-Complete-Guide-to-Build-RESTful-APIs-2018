a259b7d1db706cb20d085ba9795dca0830ea8708
sayantan007pal <sayantanpal100@gmail.com>
2025-05-13 19:17:23 +0530

    21. extending event emitter -- Refactor logger to use class-based event handling and improve event emission clarity

--- _2_Node_Module_system/app.js
+++ _2_Node_Module_system/app.js
@@ -1,21 +1,21 @@
-const EventEmitter = require("events"); //EventEmitter is a class like -Human and 'E' in EventEmitter in the beginning signifies it is a "Class"
+const EventEmitter = require('events'); //EventEmitter is a class like -Human and 'E' in EventEmitter in the beginning signifies it is a "Class"
 
-const emitter = new EventEmitter(); //event is a object like - John i.e human name
+
+
+
+
+
+const Logger = require("./logger");
+const logger = new Logger();
 
 
 //register a listener
-emitter.on('messageLogged', (arg)=>{
+logger.on('messageLogged', (arg)=>{
     console.log("Listener called ", arg);  //"Listener called " output signifies when " emitter.emit('messageLogged', {id:1, url: 'http://'}) " this event is raised , " emitter.on('messageLogged', function() " this callback function is called
 })//emitter.addListener('Name of event','Callback function or Listener') same as emitter.on()
 
 
-/*
-but here in this case the event listner above was not called since we are calling event listner from logger.js file
-
-*/
 
 
-const log = require("./logger");
-// log(); //no output
-log();      //message
-log("messag");  //message
+logger.log();      //message   Listener called  { id: 1, url: 'http://' }
+logger.log('mgdsrt');  //message    Listener called  { id: 1, url: 'http://' }
--- _2_Node_Module_system/logger.js
+++ _2_Node_Module_system/logger.js
@@ -1,14 +1,16 @@
-const EventEmitter = require("events"); //EventEmitter is a class like -Human and 'E' in EventEmitter in the beginning signifies it is a "Class"
+const EventEmitter = require('events'); //EventEmitter is a class like -Human and 'E' in EventEmitter in the beginning signifies it is a "Class"
 
-const emitter = new EventEmitter(); //event is a object like - John i.e human name
 var url = 'https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox/FMfcgzQbfBmrXJsDgGRCNtQfCjWKlwxH'  ;
 
-function show(val){
+class Logger extends EventEmitter {
+    log(val){  // show(val) was giving error as --> The error TypeError: logger.log is not a function indicates that the log method is not defined on the logger object. This issue likely stems from the logger module (./logger) not exporting the log method correctly.
     //Send an HTTP request
     console.log('message')
 
     //raised an event
-    emitter.emit('messageLogged', {id:1, url: 'http://'})//"event arguments are stored in a object"
+    this.emit('messageLogged', {id:1, url: 'http://'})//"event arguments are stored in a object"
 
+    }
 }
-module.exports = show;
+
+module.exports = Logger;
