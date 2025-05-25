541132374db5f5e33e27cfa0d1c63df832ddbea6
sayantan007pal <sayantanpal100@gmail.com>
2025-05-26 00:39:29 +0530

    44.Building your first web app

--- Sec_4_Building_RESTful_API_using_express/express-demo/index.js
+++ Sec_4_Building_RESTful_API_using_express/express-demo/index.js
@@ -5,5 +5,8 @@ app.get("/", (req, res)=>
 {
     res.send("Hello fuckers");
 });
+app.get("/api/courses", (req,res)=>{
+    res.send([1,2,3,4]);
 
+});
 app.listen(3000, ()=>console.log("Listening on port 3000"))
