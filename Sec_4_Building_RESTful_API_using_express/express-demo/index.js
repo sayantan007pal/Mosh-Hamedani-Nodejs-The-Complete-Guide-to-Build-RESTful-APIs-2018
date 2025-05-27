a864809160e9f96d05950e17bff92acd092a0882
sayantan007pal <sayantanpal100@gmail.com>
2025-05-28 02:38:41 +0530

    51. Input Validation

--- Sec_4_Building_RESTful_API_using_express/express-demo/index.js
+++ Sec_4_Building_RESTful_API_using_express/express-demo/index.js
@@ -1,3 +1,4 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
 
@@ -28,9 +29,74 @@ app.get('/api/courses/:id', (req,res) =>{
 
 
 app.post("/api/courses", (req,res)=>{
+    const schema =  Joi.object({         // Here, Joi.object({...}) creates a schema object that specifies the expected structure and validation rules for the request body.
+        name: Joi.string().min(3).required()    //It specifies that the name field must be a string, must have at least 3 characters (min(3)), and is required (required()). If the incoming request body does not include a name property, or if the value is not a string with at least 3 characters, Joi will return a validation error. 
+    });
+
+    const result = schema.validate(req.body);    /* Here, schema.validate(req.body) checks if the data sent by the client (in req.body) matches the rules specified in the schema. The result is an object that contains two main properties:
+                                                value: the validated (and possibly transformed) data
+                                                error: details about any validation errors that occurred */
+    // console.log(result);
+/*
+            INPUT
+            {}
+    OUTPUT
+{
+  value: {},
+  error: [Error [ValidationError]: "name" is required] {
+    _original: {},
+    details: [ [Object] ]
+  }
+}
+*/
+
+                        // if (!req.body.name || req.body.name.length<3) //replaced by
+    if (result.error) 
+        {
+            //400 bad request
+                         // res.status(400).send('Name is required and name should me minimum 3 character') //replaced by
+            
+             res.status(400).send(result.error);
+            /*
+            INPUT
+            {}
+            OUTPUT
+            {
+                "_original": {},
+                "details": [
+                    {
+                        "message": "\"name\" is required",
+                        "path": [
+                            "name"
+                        ],
+                        "type": "any.required",
+                        "context": {
+                            "label": "name",
+                            "key": "name"
+                        }
+                    }
+                ]
+            }
+            */
+            
+            
+             res.status(400).send(result.error.details[0].message);  
+            
+            /*
+            INPUT
+            {}
+            OUTPUT
+            "name" is required 
+            */
+
+
+            return;
+
+        }
+
     const course = {
         id : courses.length+1,
-        name : req.body.name, //  assigns the value from the request body's "name" property to the "name" property of the new course object.
+        name : req.body.name //  assigns the value from the request body's "name" property to the "name" property of the new course object.
     };
     courses.push(course); //The operation modifies the original courses array directly (it's a mutable operation), which means after execution, the courses array will contain all its previous elements plus this new course element at the end.
     res.send(course);
