d60e6a703c90c472e77aceb23d584d0dc13f4987
sayantan007pal <sayantanpal100@gmail.com>
2025-05-27 12:39:34 +0530

    47.Route Parameters

--- Sec_4_Building_RESTful_API_using_express/express-demo/index.js
+++ Sec_4_Building_RESTful_API_using_express/express-demo/index.js
@@ -10,6 +10,19 @@ app.get("/api/courses", (req,res)=>{
 
 });
 
+app.get('/api/courses/:id', (req,res) =>{
+    res.send(req.params.id);  //http://localhost:3000/api/courses/300
+});
+
+
+app.get('/api/courses/:year/:month', (req, res) =>{
+    res.send(req.params); //http://localhost:3000/api/courses/2035/1
+});
+
+app.get('/api/posts/:year/:month', (req, res) =>{
+    res.send(req.query); //http://localhost:3000/api/posts/2035/1?sortBy=name
+});
+
 //dynamic PORT
 const port = process.env.PORT || 3000;
 
