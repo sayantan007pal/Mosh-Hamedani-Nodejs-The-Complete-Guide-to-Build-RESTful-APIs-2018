77463f1c05bef538181c034163d8989a296040d4
sayantan007pal <sayantanpal100@gmail.com>
2025-05-27 18:30:51 +0530

    49. Handling POST requests and 50.Calling Endpoints using postman

--- Sec_4_Building_RESTful_API_using_express/express-demo/index.js
+++ Sec_4_Building_RESTful_API_using_express/express-demo/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const courses = [
     {id: 1, name: "course1"},
     {id: 2, name:"course2"},
@@ -23,6 +25,32 @@ app.get('/api/courses/:id', (req,res) =>{
     res.send(course); //http://localhost:3000/api/courses/1
 });
 
+
+
+app.post("/api/courses", (req,res)=>{
+    const course = {
+        id : courses.length+1,
+        name : req.body.name, //  assigns the value from the request body's "name" property to the "name" property of the new course object.
+    };
+    courses.push(course); //The operation modifies the original courses array directly (it's a mutable operation), which means after execution, the courses array will contain all its previous elements plus this new course element at the end.
+    res.send(course);
+});
+
+/*
+POST    http://localhost:3000/api/courses/
+
+input
+
+{
+    "name":"new course"
+}
+output
+
+{
+    "id": 4,
+    "name": "new course"
+}
+*/ 
 
 
 //dynamic PORT
